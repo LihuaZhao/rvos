46379b572594a55bf84e81aca08446333f56ece2
Lihua Zhao <Lihua.Zhao@windriver.com>
2022-12-06 17:08:20 +0800

    enable address exception captured in M and passthought to S

--- src/main.rs
+++ src/main.rs
@@ -106,9 +106,8 @@ pub fn id_map_range(root: &mut page::Table,
                     bits: usize)
 {
 	let mut memaddr = start & !(page::PAGE_SIZE - 1);
-	let num_kb_pages = (page::align_val(end, 12)
-	                 - memaddr)
-	                / page::PAGE_SIZE;
+	let num_kb_pages =
+		(page::align_val(end, 12) - memaddr) / page::PAGE_SIZE;
 
 	// I named this num_kb_pages for future expansion when
 	// I decide to allow for GiB (2^30) and 2MiB (2^21) page
@@ -123,7 +122,7 @@ pub fn id_map_range(root: &mut page::Table,
 // / ENTRY POINT
 // ///////////////////////////////////
 #[no_mangle]
-extern "C" fn kinit() ->usize {
+extern "C" fn kinit() {
 	// We created kinit, which runs in super-duper mode
 	// 3 called "machine mode".
 	// The job of kinit() is to get us into supervisor mode
@@ -146,22 +145,31 @@ extern "C" fn kinit() ->usize {
 		println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
 		println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
 		println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
-		println!("STACK:  0x{:x} -> 0x{:x}", KERNEL_STACK_START, KERNEL_STACK_END);
-		println!("HEAP:   0x{:x} -> 0x{:x}", kheap_head, kheap_head + total_pages * 4096);
+		println!(
+		         "STACK:  0x{:x} -> 0x{:x}",
+		         KERNEL_STACK_START, KERNEL_STACK_END
+		);
+		println!(
+		         "HEAP:   0x{:x} -> 0x{:x}",
+		         kheap_head,
+		         kheap_head + total_pages * page::PAGE_SIZE
+		);
 	}
 	id_map_range(
 	             &mut root,
 	             kheap_head,
-	             kheap_head + total_pages * 4096,
+	             kheap_head + total_pages * page::PAGE_SIZE,
 	             page::EntryBits::ReadWrite.val(),
 	);
+	// Using statics is inherently unsafe.
 	unsafe {
 		// Map heap descriptors
 		let num_pages = HEAP_SIZE / page::PAGE_SIZE;
-		id_map_range(&mut root,
-					 HEAP_START,
-					 HEAP_START + num_pages,
-					 page::EntryBits::ReadWrite.val()
+		id_map_range(
+		             &mut root,
+		             HEAP_START,
+		             HEAP_START + num_pages,
+		             page::EntryBits::ReadWrite.val(),
 		);
 		// Map executable section
 		id_map_range(
@@ -204,38 +212,20 @@ extern "C" fn kinit() ->usize {
 	}
 
 	// UART
-	page::map(
-	          &mut root,
-	          0x1000_0000,
-	          0x1000_0000,
-	          page::EntryBits::ReadWrite.val(),
-			  0
+	id_map_range(
+	             &mut root,
+	             0x1000_0000,
+	             0x1000_0100,
+	             page::EntryBits::ReadWrite.val(),
 	);
 
 	// CLINT
 	//  -> MSIP
-	page::map(
-	          &mut root,
-	          0x0200_0000,
-	          0x0200_0000,
-	          page::EntryBits::ReadWrite.val(),
-			  0
-	);
-	//  -> MTIMECMP
-	page::map(
-	          &mut root,
-	          0x0200_b000,
-	          0x0200_b000,
-	          page::EntryBits::ReadWrite.val(),
-			  0
-	);
-	//  -> MTIME
-	page::map(
-	          &mut root,
-	          0x0200_c000,
-	          0x0200_c000,
-	          page::EntryBits::ReadWrite.val(),
-			  0
+	id_map_range(
+	             &mut root,
+	             0x0200_0000,
+	             0x0200_ffff,
+	             page::EntryBits::ReadWrite.val(),
 	);
 	// PLIC
 	id_map_range(
@@ -264,18 +254,62 @@ extern "C" fn kinit() ->usize {
 	// we return. root_u is the root page table's address. When stored into
 	// the SATP register, this is divided by 4 KiB (right shift by 12 bits).
 	// We enable the MMU by setting mode 8. Bits 63, 62, 61, 60 determine
-	// the mode. 
+	// the mode.
 	// 0 = Bare (no translation)
 	// 8 = Sv39
 	// 9 = Sv48
+	// build_satp has these parameters: mode, asid, page table address.
+	let satp_value = cpu::build_satp(cpu::SatpMode::Sv39, 0, root_u);
 	unsafe {
-		// We have to store the kernel's table. The tables will be moved back
-		// and forth between the kernel's table and user applicatons' tables.
-		KERNEL_TABLE = root_u;
+		// We have to store the kernel's table. The tables will be moved
+		// back and forth between the kernel's table and user
+		// applicatons' tables. Note that we're writing the physical address
+		// of the trap frame.
+		cpu::mscratch_write(
+		                    (&mut cpu::KERNEL_TRAP_FRAME[0]
+		                     as *mut cpu::TrapFrame)
+		                    as usize,
+		);
+		cpu::sscratch_write(cpu::mscratch_read());
+		cpu::KERNEL_TRAP_FRAME[0].satp = satp_value;
+		// Move the stack pointer to the very bottom. The stack is
+		// actually in a non-mapped page. The stack is decrement-before
+		// push and increment after pop. Therefore, the stack will be
+		// allocated (decremented) before it is stored.
+		cpu::KERNEL_TRAP_FRAME[0].trap_stack =
+			page::zalloc(1).add(page::PAGE_SIZE);
+		id_map_range(
+		             &mut root,
+		             cpu::KERNEL_TRAP_FRAME[0].trap_stack
+		                                      .sub(page::PAGE_SIZE,)
+		             as usize,
+		             cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize,
+		             page::EntryBits::ReadWrite.val(),
+		);
+		// The trap frame itself is stored in the mscratch register.
+		id_map_range(
+		             &mut root,
+		             cpu::mscratch_read(),
+		             cpu::mscratch_read()
+		             + core::mem::size_of::<cpu::TrapFrame,>(),
+		             page::EntryBits::ReadWrite.val(),
+		);
+		page::print_page_allocations();
+		let p = cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize - 1;
+		let m = page::virt_to_phys(&root, p).unwrap_or(0);
+		println!("Walk 0x{:x} = 0x{:x}", p, m);
 	}
-	// table / 4096    Sv39 mode
-	(root_u >> 12)  | (8 << 60)
+	// The following shows how we're going to walk to translate a virtual
+	// address into a physical address. We will use this whenever a user
+	// space application requires services. Since the user space application
+	// only knows virtual addresses, we have to translate silently behind
+	// the scenes.
+	println!("Setting 0x{:x}", satp_value);
+	println!("Scratch reg = 0x{:x}", cpu::mscratch_read());
+	cpu::satp_write(satp_value);
+	cpu::satp_fence_asid(0);
 }
+
 
 #[no_mangle]
 extern "C" fn kmain() {
@@ -300,6 +334,21 @@ extern "C" fn kmain() {
 		let sparkle_heart = String::from_utf8(sparkle_heart).unwrap();
 		println!("String = {}", sparkle_heart);
 	}
+
+unsafe {
+	// Set the next machine timer to fire.
+	let mtimecmp = 0x0200_4000 as *mut u64;
+	let mtime = 0x0200_bff8 as *const u64;
+	// The frequency given by QEMU is 10_000_000 Hz, so this sets
+	// the next interrupt to fire one second from now.
+	mtimecmp.write_volatile(mtime.read_volatile() + 10_000_000);
+
+	// Let's cause a page fault and see what happens. This should trap
+	// to m_trap under trap.rs
+	let v = 0x0 as *mut u64;
+	v.write_volatile(0);
+}
+
 	// If we get here, the Box, vec, and String should all be freed since
 	// they go out of scope. This calls their "Drop" trait.
 	// Now see if we can read stuff:
@@ -338,6 +387,8 @@ extern "C" fn kinit_hart(_hartid: usize) {
 
 
 mod assembly;
+mod cpu;
 mod kmem;
 mod page;
 mod uart;
+mod trap;
